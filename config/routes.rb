c96122b8e2e7b4c7e41d69df42954af0bace8685
Hunter Kassow <hunterkassow@protonmail.com>
2022-08-29 17:02:22 -0400

    react frontend setup

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+
+  #rerouting to home path
+  get '*path',
+      to: 'fallback#index',
+      constraints: ->(req) { !req.xhr? && req.format.html? }
 end
