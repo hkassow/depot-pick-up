b2deafb9300322592b496769c721d39304357840
Julian Leal <julianleal1997@gmail.com>
2022-09-06 10:12:22 -0500

    checkout works

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
   get '/me', to: 'user#show'
+  get '/checkout/:id', to: "carts#checkout" 
   
   #rerouting to home path
   get '*path',
