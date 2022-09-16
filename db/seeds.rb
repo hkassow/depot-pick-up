e27160b7194dd000ca8bd6e8a1c3e6df7df48a97
Hunter Kassow <hunterkassow@protonmail.com>
2022-09-16 09:32:35 -0400

    removed aws prod files, added item seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "seeding items"
+
+
+
+10.times{|i|
+    Item.create!(name: "test-item #{i+1}", description: "test items always in stock :)", instock: true , quantity: 1, price: i*10)
+}
